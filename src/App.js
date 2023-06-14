301e587e56fdb8b02a62af5c527e693a9cac20d3
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-14 19:30:50 +0200

    add function to create new data object

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Form from "./components/form/From";
 import List from "./components/list/List";
 
 const initiateData = [
-  { id: 1, title: "Go to gym and do exercise", date: "2023 12 24", number: 1 },
-  { id: 2, title: "Practice yoga and tel mom", date: "2023 11 04", number: 2 },
-  { id: 3, title: "Study harder and hared!", date: "2023 11 14", number: 3 },
-  { id: 4, title: "Go to shopping mall", date: "2023 11 03", number: 4 },
-  { id: 5, title: "Go to gym and as petters", date: "2023 11 01", number: 5 },
+  { id: 1, title: "Go to gym and do exercise", date: "2023 12 24", number: 1, tagColor: "#fdbf11", bgColor: "#fdbf11" },
+  { id: 2, title: "Practice yoga and tel mom", date: "2023 11 04", number: 2, tagColor: "#6c56c2", bgColor: "#ff7191" },
+  { id: 3, title: "Study harder and hared!", date: "2023 11 14", number: 3, tagColor: "#fdbf11", bgColor: "#6c56c2" },
+  { id: 4, title: "Go to shopping mall", date: "2023 11 03", number: 4, tagColor: "#ff7191", bgColor: "#fdbf11" },
+  { id: 5, title: "Go to gym and as petters", date: "2023 11 01", number: 5, tagColor: "#ff7191", bgColor: "#6c56c2" },
 ];
 
 function App() {
--- src/components/form/From.js
+++ src/components/form/From.js
@@ -5,7 +5,9 @@ import "../../style/form/Form.css";
 import { BsPlus } from "react-icons/bs";
 const Form = () => {
   const [randomColor, setRandomColor] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
 
+  // function of random color picker ----------------------
   const randomNumber = Math.trunc(Math.random() * 2);
   const arrayColor = ["#fdbf11", "#6c56c2", "#ff7191"];
 
@@ -14,12 +16,24 @@ const Form = () => {
     setRandomColor(() => {
       setRandomColor(arrayColor[randomNumber]);
     });
-    console.log(randomColor);
+    const newData = {
+      id: (Math.random() * 100 + Math.random() * 50).toFixed(3),
+      title: enteredTitle,
+      date: new Date(),
+      tagColor: randomColor,
+      bgColor: randomColor,
+    };
+    console.log(newData);
+  };
+
+  // Function Input
+  const inputChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
   };
 
   return (
     <form className="form" onSubmit={ColorPickHandler}>
-      <input type="text" className="form__input" placeholder="What is in your mind?" />
+      <input type="text" className="form__input" placeholder="What is in your mind?" onChange={inputChangeHandler} />
       <button type="submit" className="form__btn--submit hover">
         <BsPlus className="btn-icon" />
       </button>
