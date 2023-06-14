fbc1d6c7999e4e505d53f1f193580dcd1fec49e1
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-14 19:12:45 +0200

    add function of pick random color

--- src/components/form/From.js
+++ src/components/form/From.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../../style/form/Form.css";
 
 import { BsPlus } from "react-icons/bs";
 const Form = () => {
+  const [randomColor, setRandomColor] = useState("");
+
+  const randomNumber = Math.trunc(Math.random() * 2);
+  const arrayColor = ["#fdbf11", "#6c56c2", "#ff7191"];
+
+  const ColorPickHandler = (event) => {
+    event.preventDefault();
+    setRandomColor(() => {
+      setRandomColor(arrayColor[randomNumber]);
+    });
+    console.log(randomColor);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={ColorPickHandler}>
       <input type="text" className="form__input" placeholder="What is in your mind?" />
       <button type="submit" className="form__btn--submit hover">
         <BsPlus className="btn-icon" />
